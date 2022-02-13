bad724fb16a1cb1b4f3435b51f7e258c6e54533a
Michael Kendrick <michael@locumsmart.net>
2022-02-13 12:12:08 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -96,6 +96,12 @@ for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(enemyNames[i]);
+    if (playerHealth > 0 && i < enemyNames.length -1) {
+        var storeConfirm = window.confirm ("The fight is over, visit the store before the next round?")
+        if (storeConfirm) {
+        shop ();
+        }
+    }
 }
     else {
         window.alert("You have lost your robot in battle! Game Over!");
@@ -119,5 +125,48 @@ var endGame = function () {
         window.alert("Thank you for playing Robot Gladiators! Come back soon");
     }
 }
+var shop = function () {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+
+            //increate health and decrease money
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+            //increase attack and decrease money
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.");
+
+            // do nothing, so function will end
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.")
+            shop ();
+            break;
+    }
+};
 startGame();
 // fight();
