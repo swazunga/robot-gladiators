d8da34d2e7b34c3728c4a5c8db57b6e577560d11
Michael Kendrick <michael@locumsmart.net>
2022-02-14 11:15:48 -0600

    1, 2, or 3

--- assets/js/game.js
+++ assets/js/game.js
@@ -122,19 +122,17 @@ var endGame = function () {
 }
 var shop = function () {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth (); 
             break;
-        case "UPGRADE":
-        case "upgrade":
-            playerInfo.upgradeAttach();
+        case 2:
+            playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // do nothing, so function will end
